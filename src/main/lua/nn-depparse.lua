8c99816aa4b425e9577e53a022f374adb5f84fbe
Emma Strubell <emma.strubell@gmail.com>
2016-05-29 17:25:29 -0400

    hyperparam model saving

--- src/main/lua/nn-depparse.lua
+++ src/main/lua/nn-depparse.lua
@@ -222,7 +222,7 @@ local function evaluate_parse(data, parser, punct)
 end
 
 local function evaluate_parse_scala()
-    os.execute('./bin/parse-fast.sh "" --test-portion ' .. params.test_portion .. '--model-file ' .. params.save_model)
+    os.execute('./bin/parse-fast.sh "" --test-portion ' .. params.test_portion .. '--model ' .. params.save_model)
 end
 
 local function test_feats(net, sent_data, decision_data, parser, punct)
